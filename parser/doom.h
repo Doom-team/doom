44a8c9b3a97ff3541505bf794c14043ccea5083c
RainbowHusk <inovatorstudio@gmail.com>
2020-12-09 21:07:32 +0300

    add parser

---
+++ parser/doom.h
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   wolf3d.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahusk <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/13 20:15:20 by ahusk             #+#    #+#             */
+/*   Updated: 2020/08/13 20:15:24 by ahusk            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DOOM_H
+#define DOOM_H
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <io.h>
+
+typedef struct s_buff
+{
+	int w;
+	int f;
+} t_buff;
+
+typedef struct s_wall
+{
+	int x1;
+	int x2;
+	int y1;
+	int y2;
+	char *texture1;
+	char *texture2;
+} t_wall;
+
+typedef struct s_parser
+{
+	t_wall *walls;
+	t_buff buff;
+	int count_walls;
+	int count_floor;
+} t_parser;
+
+void	parser(t_parser *parser);
+
+#endif
