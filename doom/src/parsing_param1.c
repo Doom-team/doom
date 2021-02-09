947f07a3293e1e885e4b72549e85d25b0ad7ed16
Gaye Rinko <grinko@oa-n3.msk.21-school.ru>
2021-02-09 21:43:10 +0300

    end of the day

--- doom/src/parsing_param1.c
+++ doom/src/parsing_param1.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing_param1.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wendell <wendell@student.42.fr>            +#+  +:+       +#+        */
+/*   By: grinko <grinko@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/28 21:50:35 by ahusk             #+#    #+#             */
-/*   Updated: 2021/02/08 20:18:01 by wendell          ###   ########.fr       */
+/*   Updated: 2021/02/09 16:17:50 by grinko           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,9 @@ SDL_Surface		*parsing_param1(t_parser *parser,
 		if (arr[1] != 0)
 		{
 			slice(sub_arr, arr[1], 1, ft_strlen(arr[1]));
-			// printf("1 %s\n", sub_arr);
 			if (!(tmp = IMG_Load(sub_arr)))
 				error((t_wolf *)parser, SDL_GetError());
-			// printf("%s\n", sub_arr);
 			tmp1 = SDL_ConvertSurfaceFormat(tmp, SDL_PIXELFORMAT_BGRA32, 0);
-			// printf("%p\n", &(texture));
 			SDL_FreeSurface(tmp);
 		}
 		else
