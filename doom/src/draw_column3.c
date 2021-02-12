f4803a7376a42a60586dd6fc7f715d8901cbe6ac
Adfazer <ded.shilov@gmail.com>
2021-02-12 16:13:19 +0300

    моя норма

---
+++ doom/src/draw_column3.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   draw_column3.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wendell <wendell@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/18 18:31:45 by skaren            #+#    #+#             */
+/*   Updated: 2021/02/09 16:20:04 by wendell          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/wolf3d.h"
+
+void			brute_draw_column_3_2(t_wolf *wolf,\
+t_point point, int count_distance, t_data_column *d)
+{
+	if (d->flag == 1)
+	{
+		d->count = (int)(round(wolf->p->\
+		walls[d->dist->number_wall[d->j]].length));
+		d->koof = (wolf->p->walls[d->dist->\
+		number_wall[d->j]].length) / d->count;
+		d->tex_1 = (wolf->p->walls[d->dist->\
+		number_wall[d->j]].length / d->count) * d->koof;
+		d->pos = d->dist->offsetx[d->j]\
+		* wolf->p->walls[d->dist->number_wall[d->j]].length;
+		d->fractpart = modf((d->pos / d->tex_1), &d->intpart);
+		d->flag = 0;
+	}
+}
+
+void			brute_draw_column_3_3(t_wolf *wolf,\
+t_point point, int count_distance, t_data_column *d)
+{
+	if (point.x == W / 2 && H / 2 < d->size && H / 2 > d->begin_y)
+	{
+		if (wolf->bon->flag_guns == 2 &&\
+		wolf->p->walls[d->dist->number_wall[d->j]].type_flag\
+		== 3 && wolf->p->walls[d->dist->number_wall[d->j]].active == 1)
+		{
+			wolf->p->walls[d->dist->number_wall[d->j]].active = 0;
+			wolf->bon->score++;
+		}
+	}
+}
